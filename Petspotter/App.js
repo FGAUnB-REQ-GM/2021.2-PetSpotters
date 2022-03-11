e7a172533436a28c079a4cfcbef3d4393bcc3dc5
antoniotoineto <netocaastro10@gmail.com>
2022-03-11 12:16:19 -0300

    teste inicial app

--- Petspotter/App.js
+++ Petspotter/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import './assets/img/eu.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Dale</Text>
+      <Text>Salve Herick Bala</Text>
+      <Image source={require('./assets/img/eu.jpg')} />
       <StatusBar style="auto" />
     </View>
   );
