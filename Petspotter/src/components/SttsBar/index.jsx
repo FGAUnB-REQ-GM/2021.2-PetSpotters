f9b2d86676ed14ac7743307cb97454abd73ac9fd
hericklima22 <herick.lima77@gmail.com>
2022-03-22 22:45:56 -0300

    componente status bar padronizado

---
+++ Petspotter/src/components/SttsBar/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StatusBar } from "react-native";
+
+function SttsBar() {
+  return (
+    <StatusBar backgroundColor="#B66C6C" />
+  )
+}
+
+export default SttsBar
