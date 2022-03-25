833c5b3fab3eebcbe490cb4c7078287921e584b3
hericklima22 <herick.lima77@gmail.com>
2022-03-25 09:58:31 -0300

    tela de perfil de pet

--- Petspotter/src/screens/PerfilPet/index.jsx
+++ Petspotter/src/screens/PerfilPet/index.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Image, TextInput, TouchableOpacity, ToastAndroid } from "react-native";
 import { ContainerView, ProfileLogo, SttsBar } from "../../components";
 import styles from "./styles"
 
 export function PerfilPet() {
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState(0)
+  const navigation = useNavigation()
   return (
     <ContainerView>
       <SttsBar />
       <ProfileLogo />
+      <View style={{marginTop: "15%"}}>
+        <Text style={styles.perfilText}>PERFIL DO PET</Text>
+      </View>
+      <View style={styles.profPicView}>
+        <Image source={require("../../../assets/img/eu.jpg")} style={{resizeMode: "center", borderRadius: 100}} />
+      </View>
+      <View style={styles.form}>
+        <View style={styles.inputBoxView}>
+          <Text style={styles.inputBoxLabel}>NOME</Text>
+          <TextInput 
+            style={styles.inputBox}
+            onChangeText={setNome}
+            value={nome}
+          />
+        </View>
+        <View style={styles.inputBoxView}>
+          <Text style={styles.inputBoxLabel}>EMAIL</Text>
+          <TextInput 
+            style={styles.inputBox}
+            onChangeText={setIdade}
+            value={idade}
+          />
+        </View>
+      </View>
+      <View style={{display: "flex", flexDirection: "row", marginTop: "20%"}}>
+        <TouchableOpacity style={styles.saveBtn} onPress={() => navigation.goBack()}>
+          <Text style={{color: "#B66C6C", fontWeight: "bold"}}>CANCELAR</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.saveBtn} onPress={() => ToastAndroid.show("Edições salvas com sucesso", 2)}>
+          <Text style={{color: "#B66C6C", fontWeight: "bold"}}>SALVAR</Text>
+        </TouchableOpacity>
+      </View>
     </ContainerView>
   );
 }
--- Petspotter/src/screens/PerfilPet/styles.js
+++ Petspotter/src/screens/PerfilPet/styles.js
@@ -1,7 +1,51 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({ 
-  
+  perfilText: {
+    fontSize: 30,
+    color: "#B66C6C",
+    fontWeight: "bold"
+  },
+  profPicView: {
+    borderColor: "#B66C6C",
+    borderWidth: 2,
+    backgroundColor: "#FFD2CE",
+    width: 130,
+    height: 130,
+    borderRadius: 80,
+    marginTop: "12%",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputBoxView: {
+    margin: 10
+  },
+  inputBox: {
+    height: 40,
+    width: 250,
+    borderWidth: 2,
+    padding: 10,
+    backgroundColor: "#FFD2CE",
+    borderRadius: 10,
+    borderColor: "#B66C6C"
+  },
+  inputBoxLabel: {
+    marginBottom: 5,
+    color: "#B66C6C",
+    fontWeight: "bold"
+  },
+  saveBtn: {
+    position: "relative",
+    borderWidth: 2,
+    backgroundColor: "#FFD2CE",
+    borderRadius: 10,
+    borderColor: "#B66C6C",
+    width: 100,
+    height: 36,
+    alignItems: "center",
+    justifyContent: "space-around",
+    margin: 23
+  }
 })
 
 export default styles
