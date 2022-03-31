8da4b88ec4d8a0763200e848fccff49c126207d1
hericklima22 <herick.lima77@gmail.com>
2022-03-31 18:11:30 -0300

    tentando ajustar a tela de perfil

--- Petspotter/src/screens/PerfilPet/index.jsx
+++ Petspotter/src/screens/PerfilPet/index.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
-import { Text, View, TouchableOpacity, FlatList, Dimensions, Image, ImageBackground } from "react-native";
-import Carousel, { Pagination } from 'react-native-snap-carousel'
+import { Text, View, TouchableOpacity, FlatList, Dimensions, Image, ImageBackground, ScrollView } from "react-native";
+import Carousel from 'react-native-snap-carousel'
 import { ContainerView, SttsBar } from "../../components"
 import styles from "./styles";
 
 export function PerfilPet() {
 const fotos = {
-  foto1: 'https://pbs.twimg.com/profile_images/1478141668159148033/IOD8SZvx_400x400.jpg',
-  foto2: 'https://i.ytimg.com/vi/hPsHAABCbRk/maxresdefault.jpg',
-  foto3: 'https://i.pinimg.com/originals/d6/a1/fd/d6a1fdbd79b80339d009aff691f49985.jpg',
-  foto4: 'https://i.ytimg.com/vi/A4_Czor6KVY/maxresdefault.jpg',
-  foto5: 'https://i.insider.com/5d124f9a9c5101048e440825?width=1000&format=jpeg&auto=webp',
-  foto6: 'https://i.ytimg.com/vi/grpIY4FIymk/hqdefault.jpg',
-  foto7: 'https://i.pinimg.com/564x/3b/92/5a/3b925aa493a467ca8ba15a4834178a38.jpg',
-  foto8: 'https://img.ibxk.com.br/2020/10/01/01115625302155.jpg?w=1040'
+  foto1: 'https://images.wallpapersden.com/image/download/husky-dog-muzzle_aWtnapSZmpqtpaSklGZlbWWtZ2llZQ.jpg',
+  foto2: 'https://mfiles.alphacoders.com/885/885461.jpg',
+  foto3: 'https://mfiles.alphacoders.com/936/thumb-1920-936778.jpg',
+  foto4: 'https://images.wallpapersden.com/image/download/nebula_am5sZ2qUmZqaraWkpJRmZW1lrWdpZWU.jpg',
+  foto5: 'https://cdn.wallpapersafari.com/38/84/dmy3p2.jpg',
+  foto6: 'https://wallpaperaccess.com/full/1139878.jpg',
+  foto7: 'https://cdn.wallpapersafari.com/83/78/K0ZSkI.jpg',
+  foto8: 'https://wallpapercave.com/wp/wp5549771.jpg'
 }
 
   const [images, setImages] = useState([
@@ -27,13 +27,12 @@ const fotos = {
     {id: '8', image: fotos.foto8},
   ])
   const { width } = Dimensions.get('window')
-  const spacing = 10
-  const thumbSize = 80
 
   const renderItem = (item, index) => {
     return <Image
       key={index}
-      style={{ width: '100%', height: '100%' }}
+      style={{ width: '100%', height: '100%'
+    }}
       resizeMode='contain'
       source={{uri: item.item.image}}
     />
@@ -42,18 +41,24 @@ const fotos = {
   return (
     <ContainerView>
       <SttsBar />
-      <View style={{margin: "5%"}}>
-        <Text style={styles.perfilText}>PERFIL DO MEU PET</Text>
-      </View>
-      <View style={styles.petPics}>
+      <View style={styles.profileImageView}>
         <Carousel
           layout="default"
           data={images}
           sliderWidth={width}
           itemWidth={width}
           renderItem={renderItem}
+          style={{position:'relative'}}
         />
+        <View style={styles.petDescriptionView}>
+          <Text style={styles.petDescription}>Mel</Text>
+          <Text style={styles.petDescription}>Fêmea</Text>
+          <Text style={styles.petDescription}>BullDog</Text>
+        </View>
       </View>
+      <ScrollView style={{}}>
+        
+      </ScrollView>
     </ContainerView>
   )
 }
--- Petspotter/src/screens/PerfilPet/styles.js
+++ Petspotter/src/screens/PerfilPet/styles.js
@@ -1,16 +1,32 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  petPics: {
-    width: '100%',
-    height: '30%',
-    backgroundColor: '#FFD2CE',
-  },
   perfilText: {
     fontSize: 30,
     color: "#B66C6C",
     fontWeight: "bold"
   },
+  profileImageView: {
+    height: '60%',
+    position: 'relative',
+    backgroundColor: '#00000043'
+  },
+  petDescriptionView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    height: '10%',
+    position: 'absolute',
+    marginTop: '130%',
+    backgroundColor: '#B66C6C43',
+    
+  },
+  petDescription: {
+    color: '#B66C6C',
+    fontSize: 24,
+    
+  }
 })
 
 export default styles
