add065d27b5730a51baa6a23aa14b7db14486131
hericklima22 <herick.lima77@gmail.com>
2022-04-05 09:13:37 -0300

    indexando as screens

--- Petspotter/src/navigation/ScreenNavigation.jsx
+++ Petspotter/src/navigation/ScreenNavigation.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { SelectPerfil } from "../screens";
-import { SignUp } from "../screens/SignUp";
-import { Initial } from "../screens/Initial";
-import { SignUpPet } from "../screens/SignUpPet";
-import { Match } from "../screens/Match";
-import { SignIn } from "../screens/SignIn";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {PerfilUserEdit, PerfilPetEdit, SelectPerfil, Teste, PerfilPet, PerfilUser} from "../screens"
+import {
+  PerfilUserEdit,
+  PerfilPetEdit,
+  SelectPerfil,
+  Teste,
+  PerfilPet,
+  PerfilUser,
+  Initial,
+  Match,
+  SignIn,
+  SignUp,
+  SignUpPet 
+} from "../screens"
+
 
 const Stack = createNativeStackNavigator();
 
