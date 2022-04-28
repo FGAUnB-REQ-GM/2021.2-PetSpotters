81dfe17b8d891012e5ecd346e5b6e9c83bc08948
hericklima22 <herick.lima77@gmail.com>
2022-04-27 23:12:22 -0300

    inserindo biblioteca swiper

--- Petspotter/src/screens/Match/index.jsx
+++ Petspotter/src/screens/Match/index.jsx
@@ -14,25 +14,79 @@ import { useUserContext } from "../../context/ContextUser";
 import { getDownloadURL, listAll, ref } from "firebase/storage";
 import { storage } from "../../../firebase";
 import styles from "./styles";
+import Swiper from "react-native-deck-swiper";
+import tw from 'tailwind-react-native-classnames'
 
 export function Match() {
   const navigation = useNavigation();
   const user = useUserContext()
   const [images, setImages] = useState([])
   const imageListRef = ref(storage, `${user.user.email}/`);
+  const [current, setCurrent] = useState({})
 
   useEffect(() => {
     setImages([])
     listAll(imageListRef).then((list) => {
       list.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
-          setImages([...images, url]);
+          // setImages(url);
+          setCurrent(
+            [
+              {
+                pic: url,
+                petName: user.user.Petnome,
+                petAge: user.user.Petidade,
+                petPath: user.user.Petendereco,
+                petBio: user.user.Petbio,
+                petRace: user.user.Petraca
+              },
+              {
+                petAge: 2,
+                petName: "dog1"
+              },
+              {
+                petAge: 3,
+                petName: "dog2"
+              },
+              {
+                petAge: 4,
+                petName: "dog3"
+              },
+              {
+                petAge: 5,
+                petName: "dog4"
+              },
+            ]
+          )
         });
       }); 
     });
   }, []);
+  
+  current ? console.log(current) : null
 
+  const DAMMY_DATA = [
+    {
+      petAge: 2,
+      petName: "dog1"
+    },
+    {
+      petAge: 3,
+      petName: "dog2"
+    },
+    {
+      petAge: 4,
+      petName: "dog3"
+    },
+    {
+      petAge: 5,
+      petName: "dog4"
+    },
+  ]
+  
   const { width } = Dimensions.get("window");
+
+
 
   const renderItem = (item, index) => {
     return (
@@ -49,36 +103,36 @@ export function Match() {
 
   const [activeSlide, setActiveSlide] = useState(0);
 
-  const paginacao = () => {
-    return (
-      <Pagination
-        dotsLength={images.length}
-        activeDotIndex={activeSlide}
-        containerStyle={{ backgroundColor: "#00000000" }}
-        dotStyle={{
-          width: 15,
-          height: 10,
-          borderRadius: 5,
-          marginHorizontal: 8,
-          backgroundColor: "#B66C6Cee",
-          shadowColor: "rgba(0, 0, 0, 0.75)",
-          shadowOffset: { width: -1, height: 1 },
-          shadowRadius: 20,
-        }}
-        inactiveDotStyle={{
-          width: 15,
-          height: 10,
-          borderRadius: 5,
-          marginHorizontal: 8,
-          backgroundColor: "rgba(255, 255, 255, 0.40)",
-          shadowColor: "rgba(0, 0, 0, 0.75)",
-          shadowOffset: { width: -1, height: 1 },
-          shadowRadius: 15,
-        }}
-        inactiveDotScale={0.6}
-      />
-    );
-  };
+  // const paginacao = () => {
+  //   return (
+  //     <Pagination
+  //       dotsLength={images.length}
+  //       activeDotIndex={activeSlide}
+  //       containerStyle={{ backgroundColor: "#00000000" }}
+  //       dotStyle={{
+  //         width: 15,
+  //         height: 10,
+  //         borderRadius: 5,
+  //         marginHorizontal: 8,
+  //         backgroundColor: "#B66C6Cee",
+  //         shadowColor: "rgba(0, 0, 0, 0.75)",
+  //         shadowOffset: { width: -1, height: 1 },
+  //         shadowRadius: 20,
+  //       }}
+  //       inactiveDotStyle={{
+  //         width: 15,
+  //         height: 10,
+  //         borderRadius: 5,
+  //         marginHorizontal: 8,
+  //         backgroundColor: "rgba(255, 255, 255, 0.40)",
+  //         shadowColor: "rgba(0, 0, 0, 0.75)",
+  //         shadowOffset: { width: -1, height: 1 },
+  //         shadowRadius: 15,
+  //       }}
+  //       inactiveDotScale={0.6}
+  //     />
+  //   );
+  // };
 
   return (
     <ContainerView>
@@ -95,7 +149,7 @@ export function Match() {
           <ProfileLogo style={{ marginTop: 5, marginBottom: 5 }} />
         </View>
       </View>
-      <View style={styles.profileImageView}>
+      {/* <View style={styles.profileImageView}>
         <Carousel
           layout="stack"
           data={images}
@@ -125,7 +179,21 @@ export function Match() {
             <Icon name="heart" size={50} color={"#33ff33ee"} />
           </TouchableOpacity>
         </View>
-      </View>
+      </View> */}
+      {current ? 
+      <View style={{width: '90%', height: '100%'}}>
+        <Swiper
+          containerStyle={{backgroundColor: "transparent"}}
+          cards={current}
+          renderCard={(pet) => (
+            <View style={{flex: 1}}>
+              <Text>{pet?.petName}</Text>
+              {console.log(pet)}
+            </View>
+          )}
+        />
+      </View> : null}
+      
     </ContainerView>
   );
 }
